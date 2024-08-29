4580ee247c8cdf30551a7a0b468094fe80cb3148
Christian Hettlage <hettlage@saao.ac.za>
2024-08-29 23:05:57 +0200

    Add a request method

--- src/pyastrosalt/requests.py
+++ src/pyastrosalt/requests.py
@@ -1,3 +1,11 @@
+from typing import Literal
+
+from requests import Response, Session as RequestsSession
+
+
+_DEFAULT_BASE_URL = "https://example.org"
+
+
 class Session:
     """
     The session for handling HTTP requests to the SALT API server.
@@ -12,10 +20,57 @@ class Session:
     When making a request with the `request`, `get`, `post`, `put`, `patch` or `delete`
     method you have to supply the API endpoint without the base URL.
     """
+
     _session: "Session" = None  # type: ignore
+    _requests_session: RequestsSession
 
     @classmethod
     def get_instance(cls) -> "Session":
         if not cls._session:
             cls._session = cls()
+            cls._session._requests_session = RequestsSession()
         return cls._session
+
+    def request(
+        self,
+        method: Literal["GET", "POST", "PUT", "PATCH", "DELETE"],
+        endpoint: str,
+        **kwargs,
+    ) -> Response:
+        """
+        Make an HTTP request to the API server.
+
+        Args:
+            method: HTTP method for the request.
+            endpoint: API endpoint, without the base URL, such as `"/status"`. The
+              endpoint must start with a single slash.
+            **kwargs: Keyword arguments, as accepted by the `request` method of the
+              `requests` library.
+
+        Returns:
+            The server response, as returned by the `request` method of the `requests`
+            library.
+
+        Raises:
+            BadRequest: The server responded with a 400 (Bad Request) error.
+            NotAuthenticated: The server responded with a 401 (Not Authorized) error.
+            Forbidden: The server responded with a 403 (Forbidden) error.
+            ServerError: The server responded with a 500 (Internal Server Error) error.
+            ValueError: The endpoint is invalid.
+        """
+        # Full URLs are not allowed.
+        if endpoint.startswith("http://") or endpoint.startswith("https://"):
+            raise ValueError(
+                "The endpoint must be the path relative to the base URL, not the URL "
+                'itself. An example would be "/status".'
+            )
+
+        # The endpoint must start with a single slash.
+        if not endpoint.startswith("/") or endpoint.startswith("//"):
+            raise ValueError(
+                "The endpoint must start with a single slash. An example would be "
+                '"/status".'
+            )
+
+        url = "https://example.org" + endpoint
+        return self._requests_session.request(method, url, **kwargs)
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pyastrosalt.requests import Session
 
 
@@ -7,3 +9,28 @@ def test_session_is_a_singleton() -> None:
 
     assert instance_1 is not None
     assert instance_1 is instance_2
+
+
+def test_request_makes_a_request_to_the_correct_url(requests_mock) -> None:
+    requests_mock.get("https://example.org/status", text="status")
+    response = Session.get_instance().request("GET", "/status")
+    assert response.status_code == 200
+    assert response.text == "status"
+
+
+@pytest.mark.parametrize(
+    "url", ("http://example.org/status", "https://example.org/status")
+)
+def test_request_rejects_full_urls(url: str) -> None:
+    with pytest.raises(ValueError, match="base URL"):
+        session = Session.get_instance()
+        session.request("GET", url)
+    assert True
+
+
+@pytest.mark.parametrize("endpoint", ("status", "//status"))
+def test_request_requires_single_leading_slash_for_endpoint(endpoint: str) -> None:
+    with pytest.raises(ValueError, match="must start with a single slash"):
+        session = Session.get_instance()
+        session.request("GET", endpoint)
+    assert True
